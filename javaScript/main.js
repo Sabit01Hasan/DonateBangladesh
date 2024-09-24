99364a9c23e1ded1630d0d48af7ab839e68d45b5
Sabit01Hasan <sabithasanifty@gmail.com>
2024-09-24 13:32:44 +0600

    added the alart when donated

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,4 +1,4 @@
-// Donate Button 1
+
 document.getElementById('d-btn1').addEventListener('click', function(event){
     event.preventDefault;
     let donation = checkInput('d-in1');
@@ -6,6 +6,10 @@ document.getElementById('d-btn1').addEventListener('click', function(event){
     document.getElementById('t-donation').innerText = total;
     let total2 = parseFloat(document.getElementById('donation1').innerText) + donation;
     document.getElementById('donation1').innerText = total2;
+    if(donation !== 0){
+        document.getElementById('alart1').style.display = 'block';
+        document.getElementById('alart2').style.display = 'block';
+    }
 
 })
 document.getElementById('d-btn2').addEventListener('click', function(event){
@@ -15,6 +19,10 @@ document.getElementById('d-btn2').addEventListener('click', function(event){
     document.getElementById('t-donation').innerText = total;
     let total2 = parseFloat(document.getElementById('donation2').innerText) + donation;
     document.getElementById('donation2').innerText = total2;
+    if(donation !== 0){
+        document.getElementById('alart1').style.display = 'block';
+        document.getElementById('alart2').style.display = 'block';
+    }
 })
 document.getElementById('d-btn3').addEventListener('click', function(event){
     event.preventDefault;
@@ -23,4 +31,13 @@ document.getElementById('d-btn3').addEventListener('click', function(event){
     document.getElementById('t-donation').innerText = total;
     let total2 = parseFloat(document.getElementById('donation3').innerText) + donation;
     document.getElementById('donation3').innerText = total2;
+    if(donation !== 0){
+        document.getElementById('alart1').style.display = 'block';
+        document.getElementById('alart2').style.display = 'block';
+    }
+})
+document.getElementById('a-btn').addEventListener('click', function(event){
+    event.preventDefault;
+    document.getElementById('alart1').style.display = 'none';
+    document.getElementById('alart2').style.display = 'none';
 })
