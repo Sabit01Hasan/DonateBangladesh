019062d0053f58f23554d5a4869ffc4634805c87
Sabit01Hasan <sabithasanifty@gmail.com>
2024-09-24 18:57:44 +0600

    some changes and bug fix

--- javaScript/main.js
+++ javaScript/main.js
@@ -2,7 +2,7 @@
 document.getElementById('d-btn1').addEventListener('click', function (event) {
     event.preventDefault;
     let donation = checkInput('d-in1');
-    let total = parseFloat(document.getElementById('t-donation').innerText) + donation;
+    let total = parseFloat(document.getElementById('t-donation').innerText) - donation;
     document.getElementById('t-donation').innerText = total;
     let total2 = parseFloat(document.getElementById('donation1').innerText) + donation;
     document.getElementById('donation1').innerText = total2;
@@ -29,7 +29,7 @@ document.getElementById('d-btn1').addEventListener('click', function (event) {
 document.getElementById('d-btn2').addEventListener('click', function (event) {
     event.preventDefault;
     let donation = checkInput('d-in2');
-    let total = parseFloat(document.getElementById('t-donation').innerText) + donation;
+    let total = parseFloat(document.getElementById('t-donation').innerText) - donation;
     document.getElementById('t-donation').innerText = total;
     let total2 = parseFloat(document.getElementById('donation2').innerText) + donation;
     document.getElementById('donation2').innerText = total2;
@@ -55,7 +55,7 @@ document.getElementById('d-btn2').addEventListener('click', function (event) {
 document.getElementById('d-btn3').addEventListener('click', function (event) {
     event.preventDefault;
     let donation = checkInput('d-in3');
-    let total = parseFloat(document.getElementById('t-donation').innerText) + donation;
+    let total = parseFloat(document.getElementById('t-donation').innerText) - donation;
     document.getElementById('t-donation').innerText = total;
     let total2 = parseFloat(document.getElementById('donation3').innerText) + donation;
     document.getElementById('donation3').innerText = total2;
